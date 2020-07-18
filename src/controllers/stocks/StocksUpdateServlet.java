6a4dd88b331b78f2d98e3563e14ac7f673cc70d8
jun.hinata <rok3war1kam2mob2043@outolook.jp>
2020-07-19 03:34:12 +0900

    Add Stock update

---
+++ src/controllers/stocks/StocksUpdateServlet.java
@@ -0,0 +1,72 @@
+package controllers.stocks;
+
+import java.io.IOException;
+import java.sql.Timestamp;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.Stock;
+import models.validators.StockValidator;
+import utils.DBUtil;
+
+/**
+ * Servlet implementation class StocksUpdateServlet
+ */
+@WebServlet("/stocks/update")
+public class StocksUpdateServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public StocksUpdateServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String _token = (String)request.getParameter("_token");
+        if(_token != null && _token.equals(request.getSession().getId())) {
+            EntityManager em = DBUtil.createEntityManager();
+
+            Stock s = em.find(Stock.class, (Integer)(request.getSession().getAttribute("stock_id")));
+
+            s.setVol(Integer.parseInt(request.getParameter("vol")));
+            s.setComment(request.getParameter("comment"));
+            s.setUpdated_at(new Timestamp(System.currentTimeMillis()));
+
+            List<String> errors = StockValidator.validate(s);
+            if(errors.size() > 0) {
+                em.close();
+
+                request.setAttribute("_token", request.getSession().getId());
+                request.setAttribute("stock", s);
+                request.setAttribute("errors", errors);
+
+                RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/stocks/edit.jsp");
+                rd.forward(request, response);
+            }
+            else {
+                em.getTransaction().begin();
+                em.getTransaction().commit();
+                em.close();
+                request.getSession().setAttribute("flush", "更新が完了しました。");
+
+                request.getSession().removeAttribute("ingredient_id");
+
+                response.sendRedirect(request.getContextPath() + "/stocks/index");
+            }
+        }
+    }
+
+}
